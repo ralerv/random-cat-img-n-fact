948052582f9c1bd5b91f224743e3926c2b318dc7
ralerv <ramos_rrv@hotmail.com>
2023-06-16 14:31:24 -0500

    JS improved

--- main.js
+++ main.js
@@ -6,24 +6,20 @@ const randomFactP = document.getElementById("random-fact");
 const apiImg = "https://cataas.com";
 const apiFact = "https://cat-fact.herokuapp.com";
 
-
 //async functions to bring data
 async function fetchData(url){ //fact data = text
     const response = await fetch(url);
     const data = await response.json();
-    randomFactP.innerText =data.text; //put random text on p
+    return data;
 }
 
-async function setcatImg(apiurl){ //randon img
-    const response = await fetch(`${apiurl}/cat?json=true`);
-    const data = await response.json();
-    const urlCat = data.url;
-    randomImgSection.src=`${apiurl}/${urlCat}`; //put random img on img.src
-}
-
-function repeat(){ //to repeat
-    fetchData(`${apiFact}/facts/random?animal_type=cat&amount=1`);
-    setcatImg(apiImg);
+async function repeat(){ //to repeat, await bc is using another async function?
+    //use fetchdata func
+    const factData = await fetchData(`${apiFact}/facts/random?animal_type=cat&amount=1`);
+    const imgData = await fetchData(`${apiImg}/cat?json=true`);
+    //put values of data
+    randomFactP.innerText =factData.text;
+    randomImgSection.src=`${apiImg}/${imgData.url}`
 }
 
 repeat(); //first fetch
