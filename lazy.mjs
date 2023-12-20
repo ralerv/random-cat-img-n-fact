9c80e7ec10ddd6c430508b4c026b85bc9216a269
ralerv <ramos_rrv@hotmail.com>
2023-12-20 15:14:43 -0500

    simplied isloaded function

--- lazy.mjs
+++ lazy.mjs
@@ -3,27 +3,22 @@ function esVisible(entries,observer){
     intersecciones.forEach(entry => {
         const container = entry.target
         const img = container.firstChild
+        img.addEventListener("load",isLoaded)
         img.src=img.dataset.src
-
-        img.addEventListener("load", (event)=>{
-            if(img.complete && img.naturalHeight !== 0){
-                container.classList.remove("w-full")
-                container.classList.add("h-auto")
-                container.style.width="auto"
-            }
-        })
-        img.removeEventListener("load", (event)=>{
-            if(img.complete && img.naturalHeight !== 0){
-                container.classList.remove("w-full")
-                container.classList.add("h-auto")
-                container.style.width="auto"
-            }
-        })
         observer.unobserve(container)
     });
 }
 
-
+function isLoaded(event){
+    let imagen = event.target
+    let contenedor = imagen.parentNode
+    if(imagen.complete && imagen.naturalHeight !== 0){
+        contenedor.classList.remove("w-full")
+        contenedor.classList.add("h-auto")
+        contenedor.style.width="auto"
+        imagen.removeEventListener("load",isLoaded)
+    }
+}
 
 const options= {}
 export const observer = new IntersectionObserver(esVisible,options);
